32b30fff9f90939651ef79592950a84c1aed6cdb
JillJimmy <jill.jimmy@proton.me>
2024-03-10 09:26:01 +0000

    Deno Deploy initial version :) 🐸💫🐸💫🐸💫✨✨

---
+++ routes/index.tsx
@@ -0,0 +1,51 @@
+export default function Home() {
+  return (
+    <div>
+      <div id="jj-head">
+      </div>
+      <div>
+        <div class="hero min-h-screen bg-none z-10">
+          <div class="hero-content text-center">
+            <div class="max-w-md">
+              <h3 class="text-3xl font-bold">This is</h3> <br class="sr-only" />
+              <h1 class="text-7xl font-black font-cond mona">
+                <span class="font-stretch-animation ">J</span>
+                <span class="font-stretch-animation animation-delay-100">
+                  i
+                </span>
+                <span class="font-stretch-animation animation-delay-200">
+                  l
+                </span>
+                <span class="font-stretch-animation animation-delay-300">
+                  l
+                </span>{" "}
+                <span class="font-stretch-animation animation-delay-400">
+                  J
+                </span>
+                <span class="font-stretch-animation animation-delay-500">
+                  i
+                </span>
+                <span class="font-stretch-animation animation-delay-600">
+                  m
+                </span>
+                <span class="font-stretch-animation animation-delay-700">
+                  m
+                </span>
+                <span class="font-stretch-animation animation-delay-800">
+                  y
+                </span>
+              </h1>
+            </div>
+          </div>
+        </div>
+        <br class="sr-only" />
+        <div class="z-20 p-3">
+          <h1 class="text-7xl mona font-cond font-black text-end">So who is Jill Jimmy, anyway?</h1>
+          <p class="z-20 text-lg">
+            Who is Jill Jimmy? Did you just say... <em>who is Jill Jimmy?</em> Wait, you seriously don't know? OK. This is crazy, but Jill Jimmy is kind of the <span class="mona font-stretch-animation font-bold">greatest superhero of all time.</span> <br /><em>Oh yeah... and he's a frog.</em>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
